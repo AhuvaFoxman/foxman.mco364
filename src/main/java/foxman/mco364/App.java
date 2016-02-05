f14275e991d81de5d216646d8e67edc257a8624b
AhuvaFoxman <afoxman36@gmail.com>
2016-02-04 20:40:44 -0500

    Intial Maven files

---
+++ src/main/java/foxman/mco364/App.java
@@ -0,0 +1,13 @@
+package foxman.mco364;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/foxman/toDo/ToDoServiceTest.java
@@ -0,0 +1,37 @@
+package foxman.toDo;
+
+import static org.junit.Assert.assertEquals;
+
+import java.io.IOException;
+import java.util.List;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import retrofit2.Call;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class ToDoServiceTest {
+	
+	@Test
+	public void testListToDos() throws IOException{
+		
+		Retrofit retrofit = new Retrofit.Builder()
+		.baseUrl("http://jsonplaceholder.typicode.com").addConverterFactory(GsonConverterFactory.create()).build();
+		
+		ToDoService service = retrofit.create(ToDoService.class);
+		
+		Call<List<Todo>> call = service.listTodos();
+		Response<List<Todo>> response = call.execute();//returns a response of listTodo
+		//can do two things with a call object
+		//execute-doing something
+		//enqueue- adding to it
+		
+		List<Todo> list = response.body();
+		Assert.assertEquals(200, list.size());
+	}
+	
+
+}
